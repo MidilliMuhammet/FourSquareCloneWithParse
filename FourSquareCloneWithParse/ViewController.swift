8e0620e268deda1f9008a8c90b88e6c75e088c79
Muhammet Midilli <midillimuhammet@gmail.com>
2021-03-14 23:30:22 +0300

    Initial Commit

---
+++ FourSquareCloneWithParse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FourSquareCloneWithParse
+//
+//  Created by Muhammet Midilli on 14.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
